3ded302ee7c43ebcbf6fd056583a41244920395f
Siam Hossain <siam1221333z@gmail.com>
2024-01-08 10:49:29 -0800

    firstvoice1

---
+++ main.py
@@ -0,0 +1,18 @@
+import pyttsx3
+voice =pyttsx3.init()
+voice.say("First question: “Why are you applying for financial aid?” (minimum 150 words)")
+voice.say("I am currently a student at GOVERNMENT ISPAHANI DEGREE COLLEGE college and I have a deep interest in the field of Machine Learning and Artificial Intelligence. I often read the popular blogs like Towards Data Science etc. ")
+voice.say("However I feel I lack the understanding of the basics of ML and hence I am not able to comprehend the articles very well. So I want to do a course that covers the fundamental topics of this field.")
+voice.say("Being a student, I have no income of myself. I could have done internships and use the stipend to pay for this course, but currently my personal and college expenses are already very high which leaves behind a very small amount as savings. With my deep interest in the field,")
+voice.say(", I don’t want to lose out the opportunity to learn, just because I am unable to pay the amount. It is a great initiative by Coursera to have a financial aid option, and hence I would like to request a financial waiver for this course.")
+voice.say("Second question: “How will taking this course help you achieve your career goals?” (minimum 150 words)")
+voice.say("It is no doubt that Machine Learning opens up lots of opportunities for one’s career. The field took off in the 2010s and has been growing ever since. ML experts and Data Scientists are some of the most well paid jobs in the world.")
+voice.say("I plan to use this course to improve my knowledge about the fundamentals of Machine Learning and Data Science. I plan to eventually specialise in Deep Learning after having learnt and practiced the basics well enough. I read that this course also includes several practice assignments and a project")
+voice.say("doing which would be immensely beneficial to the improvement of my practical skills in this area. Completing this course would also provide me the confidence to dive deep into data science and take on further challenging assignments. All of this would greatly help me in achieving my career goals in future and also help me in eventually getting a data science job.")
+voice.say("Third question: Would you like to take a non interest loan?")
+voice.say("Answer ‘No’ to this.")
+voice.say("Final question: Why would you not opt for a zero interest loan?")
+voice.say("Answer:")
+voice.say("I still have several years of education ahead of me for which I and my parents have to pay. It would be an additional and considerable expense to pay back the loan amount, due to which I am seeking a complete waiver.")
+voice.runAndWait()# firstvoice1
+# firstvoice1
